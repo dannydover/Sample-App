c28d07e56ba2b0465f6170600f92397623efad6d
Danny Dover <danny@intriguingideasllc.com>
2011-07-21 22:59:00 -0300

    Editing and updating users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -42,7 +42,6 @@ describe UsersController do
 
   end
 
-
   describe "GET 'new'" do
     it "should be successful" do
       get :new
@@ -55,14 +54,14 @@ describe UsersController do
       :content => "Sign Up")
     end
   end
-  
+
   describe "POST 'create'" do
 
-      describe "failure" do
+    describe "failure" do
 
-        before(:each) do
-          @attr = { :name => "", :email => "", :password => "",
-                    :password_confirmation => "" }
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+          :password_confirmation => "" }
         end
 
         it "should not create a user" do
@@ -81,11 +80,10 @@ describe UsersController do
           response.should render_template('new')
         end
       end
-      
+
       describe "success" do
         before(:each) do
-          @attr = { :name => "New User", :email => "user@example.com",
-                    :password => "foobar", :password_confirmation => "foobar" }
+          @attr = { :name => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" }
         end
 
         it "should create a user" do
@@ -98,16 +96,90 @@ describe UsersController do
           post :create, :user => @attr
           response.should redirect_to(user_path(assigns(:user)))
         end
-        
+
         it "should have a welcome message" do
           post :create, :user => @attr
           flash[:success].should =~ /welcome to the sample app/i
         end
-        
+
         it "should sign the user in" do
           post :create, :user => @attr
           controller.should be_signed_in
         end
       end
     end
-end
+
+    describe "GET 'edit" do
+      before(:each) do
+        @user = Factory(:user)
+        test_sign_in(@user)
+      end
+
+      it "should be successful" do
+        get :edit, :id => @user
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get :edit, :id => @user
+        response.should have_selector('title', :content => "Edit user")
+      end
+
+      it "should have a link to change the Gravatar" do
+        get :edit, :id => @user
+        response.should have_selector('a', :href => 'http://gravatar.com/emails',
+        :content => "change")
+      end
+    end
+    
+    describe "PUT 'update'" do
+
+        before(:each) do
+          @user = Factory(:user)
+          test_sign_in(@user)
+        end
+
+        describe "failure" do
+
+          before(:each) do
+            @attr = { :email => "", :name => "", :password => "",
+                      :password_confirmation => "" }
+          end
+
+          it "should render the 'edit' page" do
+            put :update, :id => @user, :user => @attr
+            response.should render_template('edit')
+          end
+
+          it "should have the right title" do
+            put :update, :id => @user, :user => @attr
+            response.should have_selector("title", :content => "Edit user")
+          end
+        end
+
+        describe "success" do
+
+          before(:each) do
+            @attr = { :name => "New Name", :email => "user@example.org",
+                      :password => "barbaz", :password_confirmation => "barbaz" }
+          end
+
+          it "should change the user's attributes" do
+            put :update, :id => @user, :user => @attr
+            @user.reload
+            @user.name.should  == @attr[:name]
+            @user.email.should == @attr[:email]
+          end
+
+          it "should redirect to the user show page" do
+            put :update, :id => @user, :user => @attr
+            response.should redirect_to(user_path(@user))
+          end
+
+          it "should have a flash message" do
+            put :update, :id => @user, :user => @attr
+            flash[:success].should =~ /updated/
+          end
+        end
+    end
+  end
